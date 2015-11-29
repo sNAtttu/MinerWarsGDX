ac125a6c8a22f7cfacb40a4e99390afb7d3470a3
sNAtttu <nsanteri@hotmail.com>
2015-11-29 22:55:19 +0200

    tiled map, movement and camera follow

--- core/src/com/minerwars/game/MinerWars.java
+++ core/src/com/minerwars/game/MinerWars.java
@@ -1,6 +1,7 @@
 package com.minerwars.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
@@ -9,60 +10,25 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import minerwars.models.Enemy;
+import minerwars.models.Player;
+import minerwars.screens.GameScreen;
+import minerwars.utils.AssetLoader;
+import minerwars.utils.Constants;
 
-public class MinerWars extends ApplicationAdapter {
 
-	private Texture playerImage;
-	private Texture enemyImage;
-	private Texture backgroundImage;
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Sprite player;
-	
+public class MinerWars extends Game {
+
 	@Override
 	public void create () {
-		playerImage = new Texture(Gdx.files.internal("Player/dwarfRight.png"));
-		enemyImage = new Texture(Gdx.files.internal("Enemy/chicken.png"));
-		backgroundImage = new Texture(Gdx.files.internal("Background/background.png"));
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1920, 1080);
-		batch = new SpriteBatch();
-		player = new Sprite(playerImage);
-		player.scale((float) -0.2);
-		initPlayer(player);
+		Gdx.app.log("MinerWars", "Created!");
+		AssetLoader.load();
+		setScreen(new GameScreen());
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		player.draw(batch);
-		batch.end();
-		checkMovement();
+	public void dispose() {
+		super.dispose();
+		AssetLoader.dispose();
 	}
-
-
-	public void initPlayer(Sprite player){
-		player.setX(100);
-		player.setY(20);
-	}
-
-	public void checkMovement(){
-		if(Gdx.input.isKeyPressed(Input.Keys.D)) {
-			player.setX(player.getX()+ 20);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.A)) {
-			player.setX(player.getX()- 20);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.W)) {
-			player.setY(player.getY() + 20);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.S)) {
-			player.setY(player.getY() - 20);
-		}
-	}
-
 }
--- desktop/src/com/minerwars/game/desktop/DesktopLauncher.java
+++ desktop/src/com/minerwars/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "MinerWars";
-		config.width = 1920;
-		config.height = 1080;
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new MinerWars(), config);
 	}
 }
