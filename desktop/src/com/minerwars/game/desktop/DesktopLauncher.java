ca2cc383e428ac98696508ede85cec04ee02d8df
sNAtttu <nsanteri@hotmail.com>
2015-11-29 11:20:48 +0200

    base project

---
+++ desktop/src/com/minerwars/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.minerwars.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.minerwars.game.MinerWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MinerWars(), config);
+	}
+}
